21fb3565210a67e4b86cd4c0c8e7922d66d48b96
Alpha <ravalasonn@gmail.com>
2019-08-30 09:26:58 +0300

    Ajout email AJAX

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,5 +1,6 @@
 class EmailController < ApplicationController
   def index
+    @emails = Email.all
   end
 
   def show
@@ -9,6 +10,15 @@ class EmailController < ApplicationController
   end
 
   def create
+    @email = Email.create(
+      object: Faker::Book.title,
+      body: Faker::Lorem.paragraph
+    )
+
+    respond_to do |format|
+      format.html{ redirect_to email_index_path }
+      format.js{ }
+    end
   end
 
   def edit
